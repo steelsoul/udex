7fce1f940868374528a8475c843da78daa89a15a
apushkar <avxa@ukr.net>
2014-12-16 23:56:49 +0200

    nAry function. # Write a function, n_ary(f), that takes a binary function (a function # that takes 2 inputs) as input and returns an n_ary function.

---
+++ Lesson 3/NAryFn/naryfn.py
@@ -0,0 +1,35 @@
+# -*- coding: windows-1251 -*-
+'''
+Created on 16 дек. 2014 г.
+
+@author: Alexander
+'''
+
+# ---------------
+# User Instructions
+#
+# Write a function, n_ary(f), that takes a binary function (a function
+# that takes 2 inputs) as input and returns an n_ary function. 
+
+
+def n_ary(f):
+    def n_ary_f(x, *args):
+        return x if not args else f(x, n_ary_f(*args))
+    return n_ary_f
+
+
+def n_ary_mine(f):
+    """Given binary function f(x, y), return an n_ary function such
+    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
+    def n_ary_f(x, *args):
+        if not args: return x
+        elif len(args) == 1: return f(x, args[0])
+        result = f(args[len(args)-2], args[len(args)-1])
+        for i in range(len(args)-1):
+            b1 = x if len(args)-2-i == 0 else args[len(args)-3-i]
+            result = f(b1, result)
+        return result
+    return n_ary_f
+
+if __name__ == '__main__':
+    pass
